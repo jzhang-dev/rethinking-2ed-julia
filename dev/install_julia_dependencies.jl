10448d8ca5d75646615512cf71eef19b1fb84830
Jia-Yuan Zhang <sloppybaron@gmail.com>
2023-01-22 16:27:39 +0000

    Update .gitpod.yml

--- dev/install_julia_dependencies.jl
+++ dev/install_julia_dependencies.jl
@@ -1,6 +1,6 @@
 using Pkg
+Pkg.instantiate()
 Pkg.add(["IJulia", "Turing", "CSV", "DataFrames", "Plots", "StatsPlots", "StatisticalRethinking"])
-
 
 using IJulia
 installkernel("Julia", "--depwarn=no")
